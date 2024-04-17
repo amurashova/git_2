edf43b1e962e88475ebace1d853d4e57bde8cd86
anastasiamurasova <nomofamilinomo@gmail.com>
2024-04-17 16:02:18 +0300

    fix conflict

--- src/main/java/ru/nomofamilinomo/Main.java
+++ src/main/java/ru/nomofamilinomo/Main.java
@@ -2,6 +2,6 @@ package ru.nomofamilinomo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa12");
+        System.out.println("Hello qa123456789");
     }
 }
