707f30b4b305480a7bd0593905df04316079864d
anastasiamurasova <nomofamilinomo@gmail.com>
2024-04-17 15:43:14 +0300

    change message2

--- src/main/java/ru/nomofamilinomo/Main.java
+++ src/main/java/ru/nomofamilinomo/Main.java
@@ -2,6 +2,6 @@ package ru.nomofamilinomo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa!");
+        System.out.println("Hello qaguru!");
     }
 }
